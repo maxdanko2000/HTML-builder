cde294c91712a5dffe1eef182d4880e9b0d609c3
maxdanko2000 <maxdanko12345@yandex.ru>
2021-11-08 20:35:38 +0300

    feat: add one more solve

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,15 +2,13 @@ const path = require('path');
 const src = './03-files-in-folder/secret-folder';
 const fs = require("fs");
 
-fs.readdir(src, function (err, items) {
-    if (!err) {
-        for (let i = 0; i < items.length; i++) {
-            fs.stat(items[i], function (err, stats) {
-
-            });
-            console.log(items[i]);
-        }
-    } else {
-        console.log(err);
+fs.readdir(src, [withFileTypes = true], function (err, items) {
+    for (let i = 0; i < items.length; i++) {
+        let file = src + '/' + items[i];
+        fs.stat(file, function (err, stats) {
+            if (stats.isFile() == true) {
+                console.log(items[i].split('.').slice(0, -1).join('.') + ' - ' + path.extname(file) + ' - ' + stats["size"] + 'kb');
+            }
+        });
     }
 });
